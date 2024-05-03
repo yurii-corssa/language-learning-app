292d513218998d7c01b65ecf45346db6b9853adf
yurii-corssa <yurii.corssa@gmail.com>
2024-05-04 00:02:01 +0200

    create modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,35 @@
 import { createPortal } from "react-dom";
+import { Backdrop, ModalBody } from "./Modal.styled";
+import XBtn from "../XBtn/XBtn";
+import { useEffect } from "react";
 
 const modalRoot = document.querySelector("#modal-root");
 
-const Modal = ({ children }) => {
-  console.log(children);
-  return createPortal(<div>{children}</div>, modalRoot);
+const Modal = ({ onClose, children }) => {
+  const handleClose = (e) => {
+    if (e.target === e.currentTarget || e.code === "Escape") {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleClose);
+
+    return () => {
+      document.removeEventListener("keydown", handleClose);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return createPortal(
+    <Backdrop onClick={handleClose}>
+      <ModalBody>
+        <XBtn onClose={onClose} />
+        {children}
+      </ModalBody>
+    </Backdrop>,
+    modalRoot
+  );
 };
 
 export default Modal;
---
+++ src/components/Modal/Modal.styled.js
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+
+export const Backdrop = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
+  padding-top: auto;
+  padding-bottom: auto;
+
+  background: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+`;
+
+export const ModalBody = styled.div`
+  position: relative;
+  padding: 64px;
+  border-radius: 30px;
+  background: #fff;
+`;
