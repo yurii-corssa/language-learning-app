b27fa07f17b1c7efa3af6931d1af984a0169590c
yurii-corssa <yurii.corssa@gmail.com>
2024-05-06 13:57:56 +0200

    create database teachers

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const app = initializeApp({
   apiKey: "AIzaSyCpuhRIrjk7iVhKTIi8C_BJxaWYVD6kyKI",
@@ -8,6 +9,8 @@ const app = initializeApp({
   storageBucket: "learn-lingo-connect.appspot.com",
   messagingSenderId: "947344857741",
   appId: "1:947344857741:web:14a89974e1b6d6d887e0c4",
+  databaseURL: "https://learn-lingo-connect-default-rtdb.europe-west1.firebasedatabase.app/",
 });
 
+export const database = getDatabase(app);
 export const auth = getAuth(app);
--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -1,4 +1,25 @@
+import { database } from "../firebaseApp";
+import { ref } from "firebase/database";
+import { useEffect } from "react";
+import { useList } from "react-firebase-hooks/database";
+
 const TeachersPage = () => {
+  const [snapshots, loading, error] = useList(ref(database));
+
+  useEffect(() => {
+    console.log(loading);
+  }, [loading]);
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  useEffect(() => {
+    snapshots.map((el) => {
+      console.log(el.key, el.val());
+    });
+  }, [snapshots]);
+
   return <div>TeachersPage</div>;
 };
 
