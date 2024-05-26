84b6d15a211a24e7b900d40901b1f9132515c2cc
yurii-corssa <yurii.corssa@gmail.com>
2024-05-26 20:04:46 +0200

    implement FavoritePage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,22 +1,40 @@
-import { useEffect } from "react";
-import { getFavorites } from "../api/users";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebaseApp";
-
-const INITIAL_DISPLAY_COUNT = 4;
+import { useFavoriteTeachers } from "../hooks/useFavoriteTeachers";
+import TeacherCard from "../components/TeacherCard/TeacherCard";
+import useFavoriteIds from "../hooks/useFavoriteIds";
 
 const FavoritesPage = () => {
   const [user] = useAuthState(auth);
+  const [favoriteIds] = useFavoriteIds(user);
+  const [favoriteTeachers, isLastPage, isLoading, error, showMore] =
+    useFavoriteTeachers(favoriteIds);
 
-  useEffect(() => {
-    const fetchFavorites = async () => {
-      const favorites = await getFavorites(user.uid);
-      console.log(favorites);
-    };
-
-    fetchFavorites();
-  }, []);
-  return <div>FavoritesPage</div>;
+  return (
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {error && <p>Error: {error.message}</p>}
+          {!favoriteTeachers.length && (
+            <p>You don&apos;t have any favorite teachers yet. Add some to see them here!</p>
+          )}
+          <ul>
+            {favoriteTeachers.map((el) => {
+              return (
+                <TeacherCard key={el.tid} teacherData={el} user={user} favoriteIds={favoriteIds} />
+              );
+            })}
+          </ul>
+          {isLastPage && favoriteTeachers.length && (
+            <p>You have reached the end of the list of teachers.</p>
+          )}
+          {!isLastPage && favoriteTeachers.length && <button onClick={showMore}>Load more</button>}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FavoritesPage;
