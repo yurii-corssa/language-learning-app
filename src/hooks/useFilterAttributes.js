51789a8fce7eb1cf6b4181d2b91028077288992f
yurii-corssa <yurii.corssa@gmail.com>
2024-05-31 18:12:29 +0200

    add filter options loading state

--- src/hooks/useFilterAttributes.js
+++ src/hooks/useFilterAttributes.js
@@ -5,28 +5,36 @@ export const useFilterAttributes = () => {
   const [languages, setLanguages] = useState([]);
   const [levels, setLevels] = useState([]);
   const [prices, setPrices] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetch = async () => {
-      const teachers = await getAllTeachers();
+      try {
+        const teachers = await getAllTeachers();
 
-      const languagesSet = new Set();
-      const levelsSet = new Set();
-      const pricesSet = new Set();
+        const languagesSet = new Set();
+        const levelsSet = new Set();
+        const pricesSet = new Set();
 
-      teachers.forEach((teacher) => {
-        teacher.languages.forEach((lang) => languagesSet.add(lang));
-        teacher.levels.forEach((lvl) => levelsSet.add(lvl));
-        pricesSet.add(teacher.price_per_hour);
-      });
+        teachers.forEach((teacher) => {
+          teacher.languages.forEach((lang) => languagesSet.add(lang));
+          teacher.levels.forEach((lvl) => levelsSet.add(lvl));
+          pricesSet.add(teacher.price_per_hour);
+        });
 
-      setLanguages([...languagesSet]);
-      setLevels([...levelsSet]);
-      setPrices([...pricesSet]);
+        setLanguages([...languagesSet]);
+        setLevels([...levelsSet]);
+        setPrices([...pricesSet]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetch();
   }, []);
 
-  return [languages, levels, prices];
+  return [languages, levels, prices, isLoading, error];
 };
