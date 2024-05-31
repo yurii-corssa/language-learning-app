ee0e479ffbd90c453f06a4f5ffc55bc948c89e89
yurii-corssa <yurii.corssa@gmail.com>
2024-05-31 18:46:10 +0200

    implement filtering

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,57 +1,61 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebaseApp";
-import { useFavoriteTeachers } from "../hooks/useFavoriteTeachers";
 import TeacherCard from "../components/TeacherCard/TeacherCard";
-import useFavoriteIds from "../hooks/useFavoriteIds";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useTeachers } from "../hooks/useTeachers";
+import FilterForm from "../components/FilterForm/FilterForm";
 
 const FavoritesPage = () => {
   const [user] = useAuthState(auth);
-  const [favoriteIds, isLoadingIds] = useFavoriteIds(user);
-  const [teachers, isLastPage, isLoadingTchrs, error, showMore] = useFavoriteTeachers(favoriteIds);
+  const [filters, setFilters] = useState({
+    language: "",
+    level: "",
+    price: "",
+    onlyFavorites: true,
+  });
+  const [teachers, favoriteIds, isLastPage, isLoading, error, showMore] = useTeachers(filters);
   const [isEmpty, setIsEmpty] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (isLoadingIds || isLoadingTchrs) {
-      setIsLoading(true);
-    } else setIsLoading(false);
-  }, [isLoadingIds, isLoadingTchrs]);
-
-  useEffect(() => {
-    if (favoriteIds.length === 0 && !isLoading) {
+    if (teachers.length === 0 && !isLoading) {
       setIsEmpty(true);
     } else {
       setIsEmpty(false);
     }
-  }, [favoriteIds.length, isLoading]);
+  }, [isLoading, teachers.length]);
+
+  const changeFilter = useCallback((filterOptions) => {
+    setFilters((prev) => ({ ...prev, ...filterOptions }));
+  }, []);
 
   return (
-    <>
-      <div>
-        {error && <p>Error: {error.message}</p>}
+    <div>
+      <FilterForm changeFilter={changeFilter} />
 
-        {isEmpty && (
+      {error && <p>Error: {error.message}</p>}
+
+      <ul>
+        {teachers.map((el) => {
+          return (
+            <TeacherCard key={el.tid} teacherData={el} user={user} favoriteIds={favoriteIds} />
+          );
+        })}
+      </ul>
+
+      {isLoading ? (
+        <div style={{ position: "relative", top: "50%", left: "50%" }}>Loading...</div>
+      ) : isEmpty ? (
+        favoriteIds.length === 0 ? (
           <p>You don&apos;t have any favorite teachers yet. Add some to see them here!</p>
-        )}
-
-        <ul>
-          {teachers.map((el) => {
-            return (
-              <TeacherCard key={el.tid} teacherData={el} user={user} favoriteIds={favoriteIds} />
-            );
-          })}
-        </ul>
-
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : isLastPage ? (
-          <p>You have reached the end of the list of teachers.</p>
         ) : (
-          <button onClick={showMore}>Load more</button>
-        )}
-      </div>
-    </>
+          <p>No teachers found matching your filter criteria. Please try adjusting your filters.</p>
+        )
+      ) : isLastPage ? (
+        <p>You have reached the end of the list of teachers.</p>
+      ) : (
+        <button onClick={showMore}>Load more</button>
+      )}
+    </div>
   );
 };
 
--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -2,17 +2,38 @@ import TeacherCard from "../components/TeacherCard/TeacherCard";
 import { useTeachers } from "../hooks/useTeachers";
 import { auth } from "../firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
-import useFavoriteIds from "../hooks/useFavoriteIds";
+import { useCallback, useEffect, useState } from "react";
+import FilterForm from "../components/FilterForm/FilterForm";
 
 const TeachersPage = () => {
   const [user] = useAuthState(auth);
-  const [favoriteIds] = useFavoriteIds(user);
-  const [teachers, isLoading, error, showMore] = useTeachers();
+  const [filters, setFilters] = useState({
+    language: "",
+    level: "",
+    price: "",
+    onlyFavorites: false,
+  });
+  const [teachers, favoriteIds, isLastPage, isLoading, error, showMore] = useTeachers(filters);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    if (teachers.length === 0 && !isLoading) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
+  }, [isLoading, teachers.length]);
+
+  const changeFilter = useCallback((filterOptions) => {
+    setFilters((prev) => ({ ...prev, ...filterOptions }));
+  }, []);
 
   return (
     <div>
-      {isLoading && <p>Loading...</p>}
+      <FilterForm changeFilter={changeFilter} />
+
       {error && <p>Error: {error.message}</p>}
+
       <ul>
         {teachers.map((el) => {
           return (
@@ -20,7 +41,16 @@ const TeachersPage = () => {
           );
         })}
       </ul>
-      <button onClick={showMore}>Load more</button>
+
+      {isLoading ? (
+        <div style={{ position: "relative", top: "50%", left: "50%" }}>Loading...</div>
+      ) : isEmpty ? (
+        <p>No teachers found matching your filter criteria. Please try adjusting your filters.</p>
+      ) : isLastPage ? (
+        <p>You have reached the end of the list of teachers.</p>
+      ) : (
+        <button onClick={showMore}>Load more</button>
+      )}
     </div>
   );
 };
