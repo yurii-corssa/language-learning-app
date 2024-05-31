3c576b0ee40b7aabb24d852098a9b56bf2a53d03
yurii-corssa <yurii.corssa@gmail.com>
2024-05-31 03:08:50 +0200

    create getTeacherAttributes function

--- src/api/teachers.js
+++ src/api/teachers.js
@@ -7,11 +7,40 @@ export const getAllTeachers = async () => {
   const teachersRef = ref(database, "teachers");
 
   const snapshot = await get(teachersRef);
-  snapshot.forEach((teacher) => {
-    newTeachers.push({ tid: teacher.key, ...teacher.val() });
-  });
+
+  if (snapshot.exists()) {
+    snapshot.forEach((teacher) => {
+      newTeachers.push({ tid: teacher.key, ...teacher.val() });
+    });
+  }
 
   return newTeachers;
+};
+
+export const getTeacherAttributes = async () => {
+  const allLanguages = [];
+  const allLevels = [];
+  const allPrices = [];
+
+  const teachersRef = ref(database, "teachers");
+
+  const snapshot = await get(teachersRef);
+
+  if (snapshot.exists()) {
+    snapshot.forEach((teacher) => {
+      const { languages, levels, price_per_hour } = teacher.val();
+
+      allLanguages.push(...languages);
+      allLevels.push(...levels);
+      allPrices.push(price_per_hour);
+    });
+  }
+
+  const languages = [...new Set(allLanguages)];
+  const levels = [...new Set(allLevels)];
+  const prices = [...new Set(allPrices)];
+
+  return { languages, levels, prices };
 };
 
 export const addTeacher = async (teacher) => {
