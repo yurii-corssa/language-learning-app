9068d0bbfa1e3152b612f9ff345c53831181c202
yurii-corssa <yurii.corssa@gmail.com>
2024-06-01 23:31:05 +0200

    implement reexport

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,3 +1,6 @@
 export { default as LevelLabel } from "./LevelLabel/LevelLabel";
 export { default as IconBtn } from "./IconBtn/IconBtn";
 export { default as Button } from "./Button/Button";
+export { default as HomeLogo } from "./HomeLogo/HomeLogo";
+export { default as BurgerBtn } from "./BurgerBtn";
+export { default as SvgIcon } from "./SvgIcon";
---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+export { useAuth } from "./useAuth";
+export { useFavoriteIds } from "./useFavoriteIds";
+export { useFilterAttributes } from "./useFilterAttributes";
+export { useMediaQuery } from "./useMediaQuery";
+export { useModal } from "./useModal";
+export { useTeachers } from "./useTeachers";
