f9bb38c6f135e0875c54499bbc2bbcd91bf1bb8e
yurii-corssa <yurii.corssa@gmail.com>
2024-06-01 23:30:06 +0200

    create variables file

---
+++ src/styles/variables.js
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: "#F4C550",
+  backgroundApp: "#F8F8F8",
+  backgroundPage: "#FFF",
+  black: "#121417",
+  white: "#FFF",
+};
+
+export const transitions = {
+  default: "250ms cubic-bezier(0.4, 0, 0.2, 1)",
+};
+
+export const breakpoints = {
+  mobile: 576,
+  tablet: 768,
+  desktop: 1024,
+};
