95fbb4fc56d0018f12793c894318a4172ced9a33
yurii-corssa <yurii.corssa@gmail.com>
2024-06-01 23:29:09 +0200

    create useMediaQuery hook

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(query);
+    const documentChangeHandler = (e) => setMatches(e.matches);
+
+    mediaQueryList.addEventListener("change", documentChangeHandler);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", documentChangeHandler);
+    };
+  }, [query]);
+
+  return matches;
+};
