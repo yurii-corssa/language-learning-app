8409e6ac56e63f633cf59e208bef037cb471143d
yurii-corssa <yurii.corssa@gmail.com>
2024-08-02 09:35:37 +0200

    fixed style buttons

--- src/components/ui/Button/Button.styled.js
+++ src/components/ui/Button/Button.styled.js
@@ -3,14 +3,15 @@ import { transitions } from "../../../styles/variables";
 import { buttonStyles } from "./buttonStyles.styled";
 
 export const ButtonStyled = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
   font-weight: 700;
   border: none;
   border-radius: 12px;
+  margin: 2px;
   cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  justify-content: center;
   transition: ${transitions.default};
   width: ${({ $width }) => $width || "auto"};
   ${({ $variant }) => buttonStyles[$variant] || buttonStyles.primary}
